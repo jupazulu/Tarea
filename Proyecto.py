afc57dc611ec0fd1dd2619e9a2296d98f6928483
jupazulu <juanpablo99@javerianacali.edu.co>
2017-05-02 11:21:22 -0500

    sds

---
+++ Proyecto.py
@@ -0,0 +1,22 @@
+import tkinter
+
+# Crea la ventana y se asocia con la variable
+v = tkinter.Tk()
+
+
+
+# crea los widgets
+d = tkinter.Canvas(v, bg="blue", width=1024, height=768)
+boton1 = tkinter.Button(v,text="Start")
+
+
+# mustra los widgets
+d.pack()
+boton1.pack()
+
+# carga una imagen
+filename = tkinter.PhotoImage(file='fondo.png')
+
+
+
+v.mainloop()
